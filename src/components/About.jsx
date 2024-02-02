414a2f8fe1ba6daa11b3f182f12bc23b0a9b278c
Nitin Minhas <nitin.minhas@hotmail.com>
2024-02-02 02:24:37 -0500

    layout adjustment on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,62 +9,62 @@ function AboutWeb({About1, About2}) {
   const iconNames = [{
     id: 1,
     name: "HTML",
-    icon: <SiHtml5 color='#E34F26' size={85} />,
+    icon: <SiHtml5 color='#E34F26' size={65} />,
     website: "https://www.w3schools.com/html/",
   },
   {
      id: 2,
     name: "CSS",
-    icon: <SiCss3 color='#1572B6' size={85} />,
+    icon: <SiCss3 color='#1572B6' size={65} />,
     website: "https://www.w3schools.com/css/",
   },
   {
     id: 3,
     name: "Javascript",
-    icon: <SiJavascript color='#F7DF1E' size={85} />,
+    icon: <SiJavascript color='#F7DF1E' size={65} />,
     website: "https://www.javascript.com/",
   },
   {
     id: 4,
     name: "React",
-    icon: <SiReact color='#61DAFB' size={85} />,
+    icon: <SiReact color='#61DAFB' size={65} />,
     website: "https://reactjs.org/",
   },
   {
     id: 5,
     name: "Tailwind CSS",
-    icon: <SiTailwindcss color='#06B6D4' size={85} />,
+    icon: <SiTailwindcss color='#06B6D4' size={65} />,
     website: "https://tailwindcss.com/",
     
   },
   {
     id: 6,
     name: "VS Code",
-    icon: <SiVisualstudiocode color='#007ACC' size={85} />,
+    icon: <SiVisualstudiocode color='#007ACC' size={65} />,
     website: "https://code.visualstudio.com/",
   },{
     id: 7,
     name: "Firebase",
-    icon: <SiFirebase color='#FFCA28' size={85} />,
+    icon: <SiFirebase color='#FFCA28' size={65} />,
     website: "https://firebase.google.com/",
   },
   {
     id: 8,
     name: "Figma",
-    icon: <SiFigma color='#F24E1E' size={85} />,
+    icon: <SiFigma color='#F24E1E' size={65} />,
     website: "https://www.figma.com/",
   },
   {
     id: 9,
     name: "Github",
-    icon: <SiGithub color='#181717' size={85} />,
+    icon: <SiGithub color='#181717' size={65} />,
     website: "https://github.com/"
   },
   {
 
      id: 10,
     name: "Creative Cloud",
-    icon: <SiAdobecreativecloud color='#DA1F26' size={85} />,
+    icon: <SiAdobecreativecloud color='#DA1F26' size={65} />,
     website: "https://www.adobe.com/creativecloud.html",
   }  
 ]
@@ -75,15 +75,15 @@ function AboutWeb({About1, About2}) {
     <div name="About"  className='relative'>
       {/* DESKTOP VIEW START */}
       <MediaQuery minWidth={700}>
-        <div className="flex items-center justify-center py-10 h-screen w-full bg-[#0478FF]">
+        <div className="flex items-center justify-center h-screen w-full bg-[#0478FF]">
           <div className='flex items-center justify-center mx-auto w-4/12'>
-            <img src={profilePicLong} width={500} className="rounded-xl shadow-2xl shadow-black" alt="proile-pic.png"/>
+            <img src={profilePicLong} width={400} className="rounded-xl shadow-2xl shadow-black" alt="proile-pic.png"/>
           </div>
-          <div className='flex flex-col items-start justify-center mx-auto w-7/12'>
-            <div className='text-white text-5xl mb-5 font-bold'>
+          <div className='flex flex-col items-start justify-center mx-auto w-7/12 space-y-2'>
+            <div className='text-white text-5xl font-bold'>
               About Me
             </div>
-            <div className='text-white space-y-5 text-lg'>
+            <div className='text-white space-y-2 text-md'>
               <p>
                 {About1}
               </p>
@@ -92,13 +92,13 @@ function AboutWeb({About1, About2}) {
               </p>
             </div>
             <div className='flex flex-col items-center justify-center w-full'>
-              <div className='flex items-center justify-center text-4xl text-white py-5'>
+              <div className='flex items-center justify-center text-4xl text-white'>
                 <ChevronLeft htmlColor='white' style={{fontSize:'4rem'}}/> 
                 <div>Skill Set</div>
                 <ChevronRight htmlColor='white' style={{fontSize:'4rem'}}/>
               </div>  
 
-              <div className='flex flex-col items-center justify-center space-y-10  bg-white/80 px-12 py-5 rounded-xl'>
+              <div className='flex flex-col items-center justify-center py-7 space-y-5 bg-white/80 px-12 rounded-xl'>
                 {[iconNames.slice(0, 5), iconNames.slice(5, 10)].map((row, rowIndex) => (
                   <div className='flex items-center justify-between space-x-20' key={rowIndex}>
                     {row.map((icon) => (
